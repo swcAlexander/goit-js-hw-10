a9c03b164998ad91039129f1701be23f028ee46a
Alexander Klymuk <swcAlexander@gmail.com>
2023-03-28 20:46:54 +0300

    some changes and correction error

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-// Використовуй публічний API Rest Countries v2, а саме ресурс name, який 
-// повертає масив об'єктів країн, що задовольнили критерій пошуку. 
+// Використовуй публічний API Rest Countries v2, а саме ресурс name, який
+// повертає масив об'єктів країн, що задовольнили критерій пошуку.
 // Додай мінімальне оформлення елементів інтерфейсу.
 
-// Напиши функцію fetchCountries(name), яка робить HTTP-запит на ресурс name і 
-// повертає проміс з масивом країн - результатом запиту.Винеси її в окремий файл 
+// Напиши функцію fetchCountries(name), яка робить HTTP-запит на ресурс name і
+// повертає проміс з масивом країн - результатом запиту.Винеси її в окремий файл
 // fetchCountries.js і зроби іменований експорт.
 
 import './css/styles.css';
@@ -11,28 +11,25 @@ import Notiflix from 'notiflix';
 import debounce from 'lodash.debounce';
 import { fetchCountries } from './js/fetchCountries';
 
-
 const DEBOUNCE_DELAY = 300;
 const refs = {
-    inputEl: document.querySelector('#search-box'),
-    countryListEl: document.querySelector('.country-list'),
-    countryInfoEl: document.querySelector('.country-info'),
-}
+  inputEl: document.querySelector('#search-box'),
+  countryListEl: document.querySelector('.country-list'),
+  countryInfoEl: document.querySelector('.country-info'),
+};
 
 refs.inputEl.addEventListener('input', debounce(searchCountry, DEBOUNCE_DELAY));
 
-
 function searchCountry(event) {
-    
-// видаляємо пробіли:
+  // видаляємо пробіли:
   const inputValue = event.target.value.trim();
   if (!inputValue) {
     resetMarkup(refs.countryListEl);
     resetMarkup(refs.countryInfoEl);
     return;
   }
-// використовуємо створену експортовану функцію для пошуку країни за назвою 
-// на сайті 'https://restcountries.com/v3.1/'
+  // використовуємо створену експортовану функцію для пошуку країни за назвою
+  // на сайті 'https://restcountries.com/v3.1/'
   fetchCountries(inputValue)
     .then(dataCountry => {
       if (dataCountry.length > 10) {
@@ -58,7 +55,7 @@ function searchCountry(event) {
 
 // додаємо розмітку у список країн
 function markupCountryListEl(country) {
-    const markup = country
+  const markup = country
     .map(({ name, flags }) => {
       return `<li class="country-list__item">
         <img class="country-list__img" src="${flags.svg}" alt="flag" />
@@ -68,17 +65,11 @@ function markupCountryListEl(country) {
     .join('');
   return refs.countryListEl.insertAdjacentHTML('beforeend', markup);
 }
-// const countryListItemEl = document.querySelector('.country-list__item')
-// countryListItemEl.addEventListener('click', addTextToInput);
-// const valueTargetEl = countryListItemEl.value;
-// function addTextToInput(valueTargetEl) {
-//     refs.inputEl.value = valueTargetEl;
-// }
 
 // додаємо розмітку у контейнер для знайденої країни
 function murkupCountryInfoEl(country) {
-    const markup = country
-    .map(({ name, flags, capital, population, languages  }) => {
+  const markup = country
+    .map(({ name, flags, capital, population, languages }) => {
       return `<div class="country-info__flag">
     <img class="country-info__img" src="${flags.svg}" alt="flag">
     <p class="country-info__name">${name.official}</p>
@@ -91,7 +82,9 @@ function murkupCountryInfoEl(country) {
     <span class="country-info__span">${population}</span>
       </li>
       <li class="country-info__item"> <b>Languages</b>:
-    <span class="country-info__span">${Object.values(languages).join(', ')}</span>
+    <span class="country-info__span">${Object.values(languages).join(
+      ', '
+    )}</span>
       </li>
   </ul>`;
     })
@@ -101,5 +94,5 @@ function murkupCountryInfoEl(country) {
 
 // очистка розмітки
 function resetMarkup(element) {
-    element.innerHTML = '';
+  element.innerHTML = '';
 }
